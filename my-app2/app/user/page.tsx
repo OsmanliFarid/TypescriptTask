9ba9bf7d69f4317a040e75a05a0a6a561794ab2f
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-11 18:10:20 +0400

    User

--- my-app2/app/user/page.tsx
+++ my-app2/app/user/page.tsx
@@ -2,32 +2,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
-import { url } from "inspector";
 
 const User = () => {
   const url: string = process.env.NEXT_PUBLIC_API_URL!;
-  const [user, setuser] = useState([]);
+  const [user, setUser] = useState([]);
   useEffect(() => {
     axios.get(url).then(({ data }) => {
-      setuser(data);
+      setUser(data);
     });
   }, []);
 
   return (
-    <div className="p-6 grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {user.map(({ id, name, surname, phone }) => {
-        return (
-          <div
-            key={id}
-            className="bg-white rounded-lg shadow-md p-6 hover:shadow-xl transition-shadow duration-300"
-          >
-            <Link href={`/user/${id}`} className=" text-xl font-semibold mb-2">
-              {name} {surname}
-            </Link>
-            <p className="text-gray-600">📞 {phone}</p>
-          </div>
-        );
-      })}
+    <div className="p-6 grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {user.map(({ id, name, surname, phone }) => (
+        <Link
+          key={id}
+          href={`/user/${id}`}
+          className="bg-white rounded-xl shadow-lg p-6 flex flex-col justify-center items-center text-center
+                     hover:shadow-2xl hover:scale-105 transition-transform duration-300 ease-in-out"
+        >
+          <h2 className="text-lg font-semibold text-indigo-700 mb-2">
+            {name} {surname}
+          </h2>
+          <p className="text-gray-600 text-sm">{phone}</p>
+        </Link>
+      ))}
     </div>
   );
 };
